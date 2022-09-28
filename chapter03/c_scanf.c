1d11c36de36089bbaa94f0063aa24992900f3945
yeodoyun <112868373+yeodoyun@users.noreply.github.com>
2022-09-28 10:44:00 +0900

    9.28

---
+++ chapter03/c_scanf.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+// scanf 함수
+//    + 키보드로부터 값을 입력
+
+// 변수 : 
+
+int main(void) {
+
+    int age = 4;
+
+    // age 호출 => 4값
+    printf("%d\n", age); // age 4
+
+    printf("나이: ");
+    scanf("%d", &age); // age 변수의 메모리 주소 (몇번지)
+
+    printf("%d", age); // age 
+
+}
